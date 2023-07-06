64e022e4d5c77b54e851581541e02518fa0abba6
Daniel Abdi <danielabdi12@gmail.com>
2023-07-06 23:41:27 +0200

    Remove unused imports and variables

--- App.tsx
+++ App.tsx
@@ -1,6 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
 import { NativeBaseProvider, Box } from "native-base";
-import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
@@ -9,12 +7,3 @@ export default function App() {
       </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
