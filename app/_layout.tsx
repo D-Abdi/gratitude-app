0a9da06ba69838998392ab8c707420a2d7402795
Daniel Abdi <danielabdi12@gmail.com>
2023-07-06 23:55:42 +0200

    Add custom text and icons to the lay-out

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,32 @@
 import { Tabs } from 'expo-router';
+import React from "react";
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { MaterialIcons } from '@expo/vector-icons';
 
 export default function Layout() {
-    return <Tabs />;
+    return (
+        <Tabs>
+            <Tabs.Screen name="DailyZen" options={{ tabBarLabel: "Daily zen",
+                tabBarIcon: ({color, size}) => (
+                    <Ionicons name="ios-happy-outline" size={24} color="black" />
+                    )
+                }}
+            />
+            <Tabs.Screen name="Journal" options={{ tabBarLabel: "Journal",
+                tabBarIcon: ({color, size}) => (
+                    <Ionicons name="ios-journal-outline" size={24} color="black" />
+                )
+            }} />
+            <Tabs.Screen name="Calendar" options={{ tabBarLabel: "Calendar",
+                tabBarIcon: ({color, size}) => (
+                    <Ionicons name="ios-calendar-outline" size={24} color="black" />
+                )
+            }} />
+            <Tabs.Screen name="Timeline" options={{ tabBarLabel: "Timeline",
+                tabBarIcon: ({color, size}) => (
+                    <MaterialIcons name="timeline" size={24} color="black" />
+                )
+            }} />
+        </Tabs>
+    );
 }
