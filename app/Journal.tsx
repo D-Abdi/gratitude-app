ec243bad286b78297910459c66d462436f060c93
Daniel Abdi <danielabdi12@gmail.com>
2023-07-06 23:58:11 +0200

    Place the journal page first

--- app/Journal.tsx
+++ app/Journal.tsx
@@ -2,6 +2,6 @@ import { Text } from "react-native"
 
 export default function Journal() {
     return (
-        <Text>This is a home page</Text>
+        <Text>This is a journal page</Text>
     )
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,17 +6,17 @@ import { MaterialIcons } from '@expo/vector-icons';
 export default function Layout() {
     return (
         <Tabs>
+            <Tabs.Screen name="Journal" options={{ tabBarLabel: "Journal",
+                tabBarIcon: () => (
+                    <Ionicons name="ios-journal-outline" size={24} color="black" />
+                )
+            }} />
             <Tabs.Screen name="DailyZen" options={{ tabBarLabel: "Daily zen",
                 tabBarIcon: () => (
                     <Ionicons name="ios-happy-outline" size={24} color="black" />
                     )
                 }}
             />
-            <Tabs.Screen name="Journal" options={{ tabBarLabel: "Journal",
-                tabBarIcon: () => (
-                    <Ionicons name="ios-journal-outline" size={24} color="black" />
-                )
-            }} />
             <Tabs.Screen name="Calendar" options={{ tabBarLabel: "Calendar",
                 tabBarIcon: () => (
                     <Ionicons name="ios-calendar-outline" size={24} color="black" />
