49a96d08f14cab4dc2c3c86724f32ca9f0b7af8d
Daniel Abdi <danielabdi12@gmail.com>
2023-07-08 01:51:19 +0200

    Add arguments to the return

--- components/buttons/Button.tsx
+++ components/buttons/Button.tsx
@@ -11,8 +11,8 @@ export const PrimaryButton_LG  = ({ text, callBack = null, pressableStyle = null
 
 export const PrimaryButtonOutline_LG  = ({ text, callBack = null, pressableStyle = null, textStyle = null }) => {
     return (
-        <Pressable style={[globalStyles.button, globalStyles.primaryBorder, pressableStyle]}>
-            <Text style={[globalStyles.buttonText, globalStyles.primaryText, textStyle]}>Sign In</Text>
+        <Pressable onPress={callBack} style={[globalStyles.button, globalStyles.primaryBorder, pressableStyle]}>
+            <Text style={[globalStyles.buttonText, globalStyles.primaryText, textStyle]}>{text}</Text>
         </Pressable>
     )
 }
