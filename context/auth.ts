1244df956279cf45cf74f5e4560a87227f6cbb7f
Daniel Abdi <danielabdi12@gmail.com>
2023-07-07 19:13:24 +0200

    Create the authentication context

---
+++ context/auth.ts
@@ -0,0 +1,26 @@
+import { useRouter, useSegments } from "expo-router";
+import React from "react";
+
+const AuthContext = React.createContext(null);
+
+// This hook can be used to access the user info
+export function useAuth() {
+    return React.useContext(AuthContext);
+}
+
+// This hook will protect the route and redirect to login if the user is not logged in
+function useProtectedRoute(user) {
+    const segments = useSegments();
+
+    React.useEffect(() => {
+        const inAuthGroup = segments[0] === "(auth)";
+
+        // If the user is not signed in and the initial segment is not anything in the auth group, redirect to sign in
+        if (!user && !inAuthGroup) {
+            useRouter().replace("/sign-in")
+            // If the user is signed in and the initial segment is anything in the auth group, redirect to home
+        } else if (user && inAuthGroup) {
+            useRouter().replace("/")
+        }
+    }, [user, segments])
+}
